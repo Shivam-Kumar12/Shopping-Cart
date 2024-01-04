9bf485397cf17598584bb37f153399af5688ebe9
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2024-01-04 10:18:26 +0530

    Add files via upload

---
+++ src/products.jsx
@@ -0,0 +1,68 @@
+import product1 from "./assets/products/1.jpeg";
+import product2 from "./assets/products/2.jpeg";
+import product3 from "./assets/products/3.jpeg";
+import product4 from "./assets/products/4.jpeg";
+import product5 from "./assets/products/5.jpeg";
+import product6 from "./assets/products/6.jpeg";
+import product7 from "./assets/products/7.jpeg";
+import product8 from "./assets/products/8.jpeg";
+import product9 from "./assets/products/9.jpeg";
+
+const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light ",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM TEE",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "SHIRT GOLF WANG",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: " HAT",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "WWE2K23 PS5",
+    price: 299.0,
+    productImage: product9,
+  },
+];
+
+export default PRODUCTS;
